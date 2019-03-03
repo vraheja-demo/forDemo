3549079ea7ee135d8a4f5509c0d3df1412729bbb
Your Name <vraheja@chargepoint.com>
2019-03-03 11:13:07 +0530

    Extract SubList

--- src/test/java/Page/PageObjects.java
+++ src/test/java/Page/PageObjects.java
@@ -1,7 +1,6 @@
 package Page;
 
 import Base.BaseClass;
-import Util.APIUtil;
 import Util.DBUtil;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -30,29 +29,30 @@ public PageObjects(WebDriver driver){
 }
 
     /**
-     * This method check if Jason response contains any data matching the getText of any button
+     * This method check if Json response contains any data matching the getText of any button
      * if found Click on the button
      *
      */
 
 public void buttonClick(String buttonData, List<String> data){
 try {
-    if (buttonData == Button1.getText()) {
+     if (buttonData == Button1.getText()) {
         Button1.click();
-        dbConnection.InsertDocument(data);
+        List<String> firstTwoRecords = data.subList(0, 2);
+        dbConnection.InsertDocument(firstTwoRecords);
 
     } else if (buttonData == Button2.getText()) {
         Button2.click();
+        List<String> nextTwoRecords = data.subList(2, 4);
+        dbConnection.InsertDocument(nextTwoRecords);
 
     } else if (buttonData == Button2.getText()) {
         Button3.click();
+        List<String> lastTwoRecords = data.subList(4, 6);
+        dbConnection.InsertDocument(lastTwoRecords);
     }
 }catch (Exception e){
     Reporter.log("The Json Response doesn't have any text related to Buttons available");
-}
-
-
-
-}
-
+    }
+  }
 }
--- src/test/java/Script/VerifyData.java
+++ src/test/java/Script/VerifyData.java
@@ -3,24 +3,20 @@ package Script;
 import Base.BaseClass;
 import Page.PageObjects;
 import Util.APIUtil;
-import Util.DBUtil;
 import Util.PropertiesUtil;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.util.EntityUtils;
-import org.bson.Document;
 import org.json.JSONObject;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 
 public class VerifyData extends BaseClass {
@@ -69,25 +65,21 @@ public class VerifyData extends BaseClass {
         }
         else {
             String Button1 = jsontest.getString("buttonName");
+            jsonData.add(jsontest.getString("CustomerName"));
             jsonData.add(jsontest.getString("email"));
+            jsonData.add(jsontest.getString("FlightFrom"));
+            jsonData.add(jsontest.getString("FlightTo"));
             jsonData.add(jsontest.getString("address"));
             jsonData.add(jsontest.getString("Details"));
+            jsonData.add(jsontest.getString(""));
             obj.buttonClick(Button1, jsonData);
         }
-
-
-
-
-
-
-
-
-        }
+    }
 
         @AfterMethod
     public void commonStepsAfterMethod(){
-        driver.quit();
-        }
-    }
+       driver.quit();
+     }
+}
 
 
